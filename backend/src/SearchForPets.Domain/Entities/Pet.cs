a37cff482bdf23c7c6d6c3bb593c9aeae2eb7132
skip59@yandex.ru <skip59@yandex.ru>
2024-08-08 14:00:50 +0500

    Создана модель Pet

---
+++ backend/src/SearchForPets.Domain/Entities/Pet.cs
@@ -0,0 +1,24 @@
+﻿namespace SearchForPets.Domain.Entities
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string AnimalType { get; set; }
+        public string Description { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string HealthAbout { get; set; }
+        public string Address { get; set; }
+        public double Weight { get; set; }
+        public double Growth { get; set; }
+        public string Phone { get; set; }
+        public bool IsCastrated { get; set; }
+        public DateOnly BirthDay { get; set; }
+        public bool IsVaccinated { get; set; }
+        public Guid VolunteerId { get; set; }
+        public string Status { get; set; }
+        public string DetailsForHelp { get; set; }
+        public DateTime AtCreated { get; set; }
+    }
+}
