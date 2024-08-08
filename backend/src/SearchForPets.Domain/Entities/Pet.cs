2a4b49c5dc6126387ced62563c2a608dd1b731a1
skip59@yandex.ru <skip59@yandex.ru>
2024-08-08 14:15:32 +0500

    Правки по замечаниям. Вернул Nulable

--- backend/src/SearchForPets.Domain/Entities/Pet.cs
+++ backend/src/SearchForPets.Domain/Entities/Pet.cs
@@ -3,22 +3,22 @@
     public class Pet
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string AnimalType { get; set; }
-        public string Description { get; set; }
-        public string Breed { get; set; }
-        public string Color { get; set; }
-        public string HealthAbout { get; set; }
-        public string Address { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string AnimalType { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Breed { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string HealthAbout { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
         public double Weight { get; set; }
         public double Growth { get; set; }
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty;
         public bool IsCastrated { get; set; }
         public DateOnly BirthDay { get; set; }
         public bool IsVaccinated { get; set; }
         public Guid VolunteerId { get; set; }
         public Status Status { get; set; }
-        public Requisites DetailsForHelp { get; set; }
+        public Requisites? DetailsForHelp { get; set; }
         public DateTime AtCreated { get; set; }
     }
 
