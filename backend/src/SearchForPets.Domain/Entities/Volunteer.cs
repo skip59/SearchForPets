0b4b6c19147affb8442066480ca894c25b07a597
skip59@yandex.ru <skip59@yandex.ru>
2024-08-09 08:09:56 +0500

    Cоздать модель волонтёра - Volunteer

---
+++ backend/src/SearchForPets.Domain/Entities/Volunteer.cs
@@ -0,0 +1,19 @@
+﻿using SearchForPets.Domain.Entities.Common;
+
+namespace SearchForPets.Domain.Entities
+{
+    public class Volunteer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int YearsOfExperience { get; set; }
+        public int NumberOfPetAbleHome { get; set; }
+        public int NumberOfPetLookingHome { get; set; }
+        public int NumberOfPetBeingTreated { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public List<SocialNetwork> SocialNetworks { get; set; } = [];
+        public List<Requisites> Requisites { get; set; } = [];
+        public List<Pet> Pets { get; set; } = [];
+    }
+}
